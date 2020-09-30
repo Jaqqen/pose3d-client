a86ad77d88742a28af3a4daab78da9d0c622248d
Jaqqen <hussain.daniyal@outlook.com>
2020-09-30 23:47:24 +0200

    Updates

---
+++ precache-manifest.949c482083da203574e540a05b23ccb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1c90b124fdf76244f306c5bfc5a333",
+    "url": "/pose3d-client/index.html"
+  },
+  {
+    "revision": "35376d8dc4295e33d4e7",
+    "url": "/pose3d-client/static/css/main.e89b774c.chunk.css"
+  },
+  {
+    "revision": "b5ba60ed4aa7ac665499",
+    "url": "/pose3d-client/static/js/2.7e969263.chunk.js"
+  },
+  {
+    "revision": "1b6938c9921cfc128a868f960d41f143",
+    "url": "/pose3d-client/static/js/2.7e969263.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35376d8dc4295e33d4e7",
+    "url": "/pose3d-client/static/js/main.739d3007.chunk.js"
+  },
+  {
+    "revision": "fef0b20fd8961fac0648",
+    "url": "/pose3d-client/static/js/runtime-main.39d3b9cf.js"
+  },
+  {
+    "revision": "95c136575a7bb259db0fa682eb6bf54d",
+    "url": "/pose3d-client/static/media/dURRLkVfHr7ZRqTSHeHTEj-1366-80.95c13657.jpg"
+  }
+]);
